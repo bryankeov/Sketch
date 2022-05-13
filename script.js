ac8ceb8c2dc399eb813efb1f20d6b273773ddecc
Bryan <suprnewb@gmail.com>
2022-05-13 13:47:46 +1000

    Repair Grid

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ let container = document.getElementById("container")
 let div = document.createElement('div');
 div.id = "square"
 
-function createDiv(n) {
-    n *= n
-  for (let i = 0; i < n; i++) {
+function createDiv() {     //Create n amount of divs
+  for (let i = 0; i < 256; i++) {
     container.appendChild(div.cloneNode(true));
   }
 }
-createDiv(10)
+createDiv()
