cc4effa7cff32a1497445abe6b2a42136de8e9f2
Bryan <suprnewb@gmail.com>
2022-05-17 15:22:24 +1000

    Add hover effect

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-let container = document.getElementById("container")
-let div = document.createElement('div');
-div.id = "square"
+let container = document.getElementById("container");
 
-function createDiv() {     //Create n amount of divs
-  for (let i = 0; i < 256; i++) {
-    container.appendChild(div.cloneNode(true));
+function createDiv(n) {
+  for(let i = 0; i < (n*n); i++) {
+      const div = document.createElement('div');
+      div.className = 'square';
+      container.appendChild(div);
   }
 }
-createDiv()
+
+/*n *= n
+for (let i = 0; i < n; i++) {
+  container.appendChild(div.cloneNode(true));
+  */
+createDiv(32)
+
+const divEls = document.querySelectorAll('.square');
+if (divEls) {
+    divEls.forEach (el => {
+        el.addEventListener('mouseenter', (event) => {
+        const currentBg = event.target.style.background;
+        if(currentBg !== 'black') {
+            event.target.style.background = 'black';
+        } else {
+            event.target.style.background = 'black';
+        }
+    });
+});
+}
+
+function gridSize() {
+    let size = prompt('Please enter number below 100')
+    if (size <= 100) {
+
+    }
+}
+
